54562bc7880d5d0736699fb7875ba7639f262129
Edina Szabó <szabo.edina.1229@gmail.com>
2019-10-04 12:39:01 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Szinbekeres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hatterSzin = new System.Windows.Forms.ColorDialog();
+            this.szinvalaszto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.eredmeny = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.osszead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hatterSzin
+            // 
+            this.hatterSzin.AllowFullOpen = false;
+            this.hatterSzin.FullOpen = true;
+            this.hatterSzin.ShowHelp = true;
+            // 
+            // szinvalaszto
+            // 
+            this.szinvalaszto.Location = new System.Drawing.Point(78, 192);
+            this.szinvalaszto.Name = "szinvalaszto";
+            this.szinvalaszto.Size = new System.Drawing.Size(118, 23);
+            this.szinvalaszto.TabIndex = 1;
+            this.szinvalaszto.Text = "Színek";
+            this.szinvalaszto.UseVisualStyleBackColor = true;
+            this.szinvalaszto.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Válassz egy színt:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(532, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // eredmeny
+            // 
+            this.eredmeny.Location = new System.Drawing.Point(532, 268);
+            this.eredmeny.Name = "eredmeny";
+            this.eredmeny.Size = new System.Drawing.Size(100, 22);
+            this.eredmeny.TabIndex = 5;
+            this.eredmeny.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(532, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Adj meg két számot : ";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(610, 194);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Eredmény :";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // osszead
+            // 
+            this.osszead.Location = new System.Drawing.Point(532, 222);
+            this.osszead.Name = "osszead";
+            this.osszead.Size = new System.Drawing.Size(150, 23);
+            this.osszead.TabIndex = 9;
+            this.osszead.Text = "Összeadás";
+            this.osszead.UseVisualStyleBackColor = true;
+            this.osszead.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.osszead);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.eredmeny);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.szinvalaszto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog hatterSzin;
+        private System.Windows.Forms.Button szinvalaszto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox eredmeny;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button osszead;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szinbekeres
+{
+    public partial class Form1 : Form
+    {   public int a, b;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = hatterSzin.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                this.BackColor = hatterSzin.Color;
+                label1.BackColor = Color.White;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                
+            }
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            eredmeny.ReadOnly = true;
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {   
+            
+            eredmeny.Text = Convert.ToString(a+b);
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            b = Convert.ToInt32(textBox2.Text);
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(textBox1.Text);
+        }
+    }
+}
